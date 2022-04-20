d5f2ec16a2a0b5f9cabefffd603990c1606793a7
Eng-Ali Ghanme <79597045+CyberPro99@users.noreply.github.com>
2022-04-21 01:22:56 +0300

    Update Sentence Smash.py

--- 8kyu/Sentence Smash.py
+++ 8kyu/Sentence Smash.py
@@ -1,1 +1,20 @@
+word=['hello', 'world', 'this', 'is', 'great']
+# def smash(words):
+#     return print("'"+" ".join(words) + "'") 
 
+# smash(word)
+
+
+def smash(words):
+    
+    i = 0
+    l = len(words)
+    str1 = ""
+    while i < l:
+        if i < l-1:
+            str1 += words[i] + " "
+        else: 
+            str1 += words[i]
+        i += 1
+        
+    return str1
